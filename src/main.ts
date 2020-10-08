15b85d7b8c4090c062b4d2a219340aa65570a7e8
watanabe <s.watanabe@sdk-co.com>
2020-10-08 11:57:40 +0900

    移転を設定

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import Aurelia from 'aurelia';
-import { MyApp } from './my-app';
-import * as globalComponents from './components/registry';
-import * as globalValueConverters from './value-converters/registry';
-import 'flatpickr/dist/flatpickr.min.css';
+import { Jump } from './jump';
+//import * as globalComponents from './components/registry';
+//import * as globalValueConverters from './value-converters/registry';
+//import 'flatpickr/dist/flatpickr.min.css';
 
 Aurelia
-  .register(
-    globalComponents,
-    globalValueConverters
-  )
-  .app(MyApp)
+  // .register(
+  //   globalComponents,
+  //   globalValueConverters
+  // )
+  .app(Jump)
   .start();
